7a90b1a398ddc1bd6d859f57319a6ccb4f7f1665
Michael Valladolid <mvalladolid@indeed.com>
2022-11-10 18:39:12 +0900

    lua keymaps

--- .config/nvim/lua/init.lua
+++ .config/nvim/lua/init.lua
@@ -13,20 +13,4 @@ require("trouble").setup()
 
 require("lsp/config")
 require("lsp/format")
-
-local builtin = require('telescope.builtin')
-
-vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-vim.keymap.set('n', '<leader>ff', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-
-vim.keymap.set('n', '<leader>fc', builtin.git_commits, {})
-vim.keymap.set('n', '<leader>ft', builtin.git_stash, {})
-vim.keymap.set('n', '<leader>bc', builtin.git_bcommits, {})
-
-vim.keymap.set('n', '<leader>fs', builtin.lsp_workspace_symbols, {})
-vim.keymap.set('n', '<leader>bs', builtin.lsp_document_symbols, {})
-vim.keymap.set('n', '<leader>dr', builtin.lsp_references, {})
-vim.keymap.set('n', '<leader>di', builtin.lsp_implementations, {})
-vim.keymap.set('n', '<leader>dd', builtin.lsp_definitions, {})
-vim.keymap.set('n', '<leader>dd', builtin.lsp_type_definitions, {})
+require("key-mappings")
