6a1be62fe2a3e86cea00af262e57ea245e4cd5a0
Michael Valladolid <mvalladolid@indeed.com>
2023-09-14 09:49:27 +0900

    format on save

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -77,3 +77,6 @@
 (setq dap-auto-configure-mode t)
 (after! dap-mode
   (require 'dap-cpptools))
+
+(add-hook 'c-mode-hook #'format-all-mode)               ;; enable code formatting on save
+(setq-hook! 'c-mode-hook +format-with-lsp nil)
